11a1ceabaf6bcb8aae2adae4ed55aaee2eb374c8
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-07 01:49:22 +0100

    flooring using the math module

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Returns floor of a float
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """Returns the floor of the argument
+
+    Args:
+        n (float): parameter to receive a float
+    """
+    return math.floor(n)
