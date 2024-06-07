e3076def90264ab6523f9576129fbcb069deab93
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-07 05:41:35 +0100

    safely get a value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -10,6 +10,7 @@ def safely_get_value(
                         dct: Mapping,
                         key: Any,
                         default: Union[T, None] = None) -> T | None:
+    """Safely get a value from a list"""
     if key in dct:
         return dct[key]
     else:
