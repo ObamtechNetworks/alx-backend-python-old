c0b8ca9762938bc643011adf16a0b6fc6f9d99ea
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-07 02:14:14 +0100

    change a float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A function that returns a string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """returns a string representation of a float
+
+    Args:
+        n (float): parameter float
+
+    Returns:
+        str: returns a str represenation of a float
+    """
+    return str(n)
