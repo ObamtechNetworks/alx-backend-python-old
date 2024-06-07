20f2f1497f5a1bd734895ab5d4f3703f42d9291e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-07 05:01:52 +0100

    fixed bug

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Safely get a value from an iterator"""
 
-from typing import TypeVar, Any, Mapping
+from typing import TypeVar, Any, Mapping, Union
 
 T = TypeVar('T')
 
@@ -9,7 +9,7 @@ T = TypeVar('T')
 def safely_get_value(
                         dct: Mapping,
                         key: Any,
-                        default: T | None = None) -> T | None:
+                        default: Union[T, None] = None) -> T | None:
     if key in dct:
         return dct[key]
     else:
