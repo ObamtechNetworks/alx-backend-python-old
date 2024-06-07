4c85c864c37fae893f0e95d8c519cf810b86ac50
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-07 01:07:23 +0100

    files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+
+"""Basic annotations"""
+
+def add(a: float, b: float) -> float:
+    """A function that sums two floating point number
+
+    Args:
+        a (float): first parameter
+        b (float): second parameter
+
+    Returns:
+        float: Returns the sum of the arguments as float
+    """
+    return a + b;
